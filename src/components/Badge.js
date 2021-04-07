f43a84516d2e6115d059fdf75e23be65c07880c8
Rodrigo Martinez <rodrigomartinezbecerra@gmail.com>
2021-04-06 21:54:20 -0400

    [DEV] primeros estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+            alt="Avatar"
+          />
           <h1>
             Richard <br /> Kaufman
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Egineer</p>
-          <p>@yiyo</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Egineer</h3>
+          <div>@yiyo</div>
         </div>
 
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
