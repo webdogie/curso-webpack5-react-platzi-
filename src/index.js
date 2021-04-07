f26f5742ee242f33f0666a1e66a0d403f848241b
Rodrigo Martinez <rodrigomartinezbecerra@gmail.com>
2021-04-06 20:52:48 -0400

    [DEV] clase de JSX

--- src/index.js
+++ src/index.js
@@ -8,9 +8,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
-
+// const jsx = <h1>Hello, Platzi Badges!</h1>;
+// const element = React.createElement(
+//   'a',
+//   { href: 'https://platzi.com' },
+//   'Ir a platzi'
+// );
+const name = 'Juan';
+const job = 'Ingeniero frontend';
+// const element = React.createElement('h1', {}, `Hola soy ${name}`);
+// const jsx = <h1>Hola soy {name}</h1>;
 const container = document.getElementById('app');
 
+const jsx = (
+  <div>
+    <h1>Hola soy {name}</h1>
+    <p>Soy {job}</p>
+  </div>
+);
+ReactDOM.render(jsx, container);
+
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
