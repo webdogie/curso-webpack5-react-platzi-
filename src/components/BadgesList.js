5aec8587ea6887513feea5bb538c6c11d8057c70
Rodrigo Martinez <rodrigomartinezbecerra@gmail.com>
2021-04-08 19:39:27 -0400

    [DEV] lista de componentes + el manejo de info across components

---
+++ src/components/BadgesList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import './styles/BadgesList.css';
+import twitterLogo from '../images/twitter.svg';
+
+{
+  /* <li key={badge.id}>
+  <p>
+    {badge.firstName} {badge.lastName}
+  </p>
+</li>; */
+}
+
+class BadgesList extends React.Component {
+  render() {
+    return (
+      <ul className="Badge_list list-unstyled">
+        {this.props.badges.map((badge) => {
+          return (
+            <li key={badge.id} className="Badge_list__item">
+              <a href="/">
+                <div className="Badge_list__item--container">
+                  <div>
+                    <img src={badge.avatarUrl} alt="Avatar" />
+                  </div>
+                  <div className="container__info">
+                    <p>
+                      {badge.firstName} {badge.lastName}
+                    </p>
+                    <div className="container__info--twitter">
+                      <img src={twitterLogo} alt="twitterImg" width="20" />
+                      <p>@{badge.twitter}</p>
+                    </div>
+                    <p>{badge.jobTitle}</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default BadgesList;
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,32 +2,56 @@ import React from 'react';
 
 import './styles/BadgeNew.css';
 import header from '../images/badge-header.svg';
-import Navbar from '../components/Navbar';
-import BadgeForm from '../components/BadgeForm';
-
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
+import Navbar from '../components/Navbar';
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: '',
+    },
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
         <Navbar />
         <div className="BadgeNew__hero">
-          <img src={header} alt="Logo" className="img-fluid" />
+          <img className="img-fluid" src={header} alt="Logo" />
         </div>
+
         <div className="container">
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Richardo"
-                lastName="Kaufman"
-                twitter="sparragus"
-                jobTitle="Software Dev"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
               />
             </div>
+
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
