44157e1c4436b320ea9492617ea48b0effb2c828
Rodrigo Martinez <rodrigomartinezbecerra@gmail.com>
2021-04-08 21:46:24 -0400

    [DEV] manejo de interfaz con layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Layout from '../components/Layout';
 import BadgeNew from '../pages/BadgeNew';
 import Badges from '../pages/Badges';
+import NotFound from '../components/NotFound';
+import Navbar from '../components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route exact path="/badges" component={Badges} />
         <Route exact path="/badges/new" component={BadgeNew} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
