695be9cc1c82af7f5ed8c79b0b13eb94935e57c9
Rodrigo Martinez <rodrigomartinezbecerra@gmail.com>
2021-04-14 17:48:54 -0400

    [PROD] squashed bug prod 5

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/BadgesList.css';
 import twitterLogo from '../images/twitter.svg';
@@ -18,23 +19,25 @@ class BadgesList extends React.Component {
         {this.props.badges.map((badge) => {
           return (
             <li key={badge.id} className="Badge_list__item">
-              <a href="/">
-                <div className="Badge_list__item--container">
-                  <div>
-                    <img src={badge.avatarUrl} alt="Avatar" />
-                  </div>
-                  <div className="container__info">
-                    <p>
-                      {badge.firstName} {badge.lastName}
-                    </p>
-                    <div className="container__info--twitter">
-                      <img src={twitterLogo} alt="twitterImg" width="20" />
-                      <p>@{badge.twitter}</p>
+              <Link to="/react_platzi">
+                <a href="/">
+                  <div className="Badge_list__item--container">
+                    <div>
+                      <img src={badge.avatarUrl} alt="Avatar" />
                     </div>
-                    <p>{badge.jobTitle}</p>
+                    <div className="container__info">
+                      <p>
+                        {badge.firstName} {badge.lastName}
+                      </p>
+                      <div className="container__info--twitter">
+                        <img src={twitterLogo} alt="twitterImg" width="20" />
+                        <p>@{badge.twitter}</p>
+                      </div>
+                      <p>{badge.jobTitle}</p>
+                    </div>
                   </div>
-                </div>
-              </a>
+                </a>
+              </Link>
             </li>
           );
         })}
